231db44d466cba1af3c2f2e274e4e2f182042842
Flily Hsu <flily.hsu@qq.com>
2016-04-01 15:22:14 +0800

    Add raw dump

--- hascrpt.py
+++ hascrpt.py
@@ -5,11 +5,31 @@ import hashlib
 
 
 def hash_info(method, **kwargs):
+    """
+
+    :param method:
+    :type method: str
+    :param kwargs:
+    :return:
+    """
     if method == "md5":
         return md5_hash(**kwargs)
 
     elif method == "sha1":
         return sha1_hash(**kwargs)
+
+    elif method.startswith("raw"):
+        return raw_dump(method[3:].lower(), **kwargs)
+
+
+def raw_dump(method, email, password):
+    return {
+        "email": email,
+        "password": {
+            "type": method,
+            "value": password.lower(),
+        }
+    }
 
 
 def md5_hash(email, password):
